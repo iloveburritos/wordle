24eb816814d33b5219934000623a19af15a32307
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-11-16 20:22:02 +0700

    updated home page and logo

--- app/page.tsx
+++ app/page.tsx
@@ -28,9 +28,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[1fr_auto] min-h-[calc(100vh-88px)] p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 items-center sm:items-start text-center sm:text-left">
-        <h1 className="text-4xl font-bold">Welcome to on-chain Wordle!</h1>
+        <h1 className="text-4xl font-bold">Welcome to the on-chain Wordl3!</h1>
         <p className="text-sm sm:text-base">
-          An on-chain version of the popular NYT game, built with Next and for Ethereum. 
+          Share your score in a group chat, without the risk of giving away any hints or clues. Guaranteed way to know you're the best. Built with Next and for Ethereum. 
         </p>
         <div className="flex gap-4">
           <Button size="lg" onClick={() => handleAction('play')}>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,11 @@
-//components/Header.tsx
-
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { WalletButton } from './WalletModal'
-import { Button } from '@/components/ui/button'
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 py-4 bg-black bg-opacity-40 ">
+    <header className="fixed top-0 left-0 right-0 z-50 py-4 bg-black bg-opacity-40">
       <div className="container mx-auto px-4 py-4 flex flex-col md:flex-row justify-between items-center">
         <Link href="/" className="flex items-center w-full md:w-auto justify-center md:justify-start mb-4 md:mb-0">
           <Image
@@ -19,9 +16,10 @@ export default function Header() {
             className="h-auto"
             priority
           />
+          <span className="text-white text-2xl font-bold ml-2">WORDL3</span>
         </Link>
         <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6">
-          <div className="flex items-center space-x-4" >
+          <div className="flex items-center space-x-4">
             <WalletButton />
           </div>
         </div>
