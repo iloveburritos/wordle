3c2df70b88f406cd62faa95fc1775eb4cafc36d5
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-12-17 16:06:44 -0800

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,26 @@
 module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     "@typescript-eslint/no-unused-vars": ["error", {
       "argsIgnorePattern": "^_",
       "varsIgnorePattern": "^_",
       "ignoreRestSiblings": true
     }],
-    // If you want to completely disable the rule:
-    // "@typescript-eslint/no-unused-vars": "off",
+    // Disable the exhaustive-deps warning for useCallback
+    "react-hooks/exhaustive-deps": "warn",
+    // Allow imports
+    "@typescript-eslint/no-explicit-any": "warn"
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   }
 } 
