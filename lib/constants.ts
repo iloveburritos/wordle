55e2da5d5802c2f4b477bf9c53f795af1626ac35
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2025-06-14 17:11:24 -0700

    updated subgraph

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const SUBGRAPH_URL = process.env.NEXT_PUBLIC_SUBGRAPH_URL || 'https://api.studio.thegraph.com/query/94961/wordl31155v2/v0.0.1/graphql';
+export const SUBGRAPH_URL = process.env.NEXT_PUBLIC_SUBGRAPH_URL || 'https://api.studio.thegraph.com/query/113921/wordl-3/version/latest';
 
 if (!SUBGRAPH_URL) {
   throw new Error('NEXT_PUBLIC_SUBGRAPH_URL is not defined in environment variables');
